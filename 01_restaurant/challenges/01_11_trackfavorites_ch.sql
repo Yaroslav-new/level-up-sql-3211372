18b63a291eb3af5b9828a8d1e43a375e9f3222c0
Yaroslav <yaroslav.berezhnyi.nl@gmail.com>
2025-03-16 09:42:27 +0100

    Tasks 11 and 12 solved

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,25 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+SELECT DishID 
+FROM Dishes 
+WHERE Name = 'Quinoa Salmon Salad'
+LIMIT 1;
+
+SELECT CustomerID, FirstName, LastName, FavoriteDish 
+FROM Customers 
+WHERE FirstName = 'Cleo' 
+  AND LastName = 'Goldwater'
+LIMIT 1;
+
+UPDATE Customers 
+SET FavoriteDish = (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad' LIMIT 1)
+WHERE FirstName = 'Cleo' 
+  AND LastName = 'Goldwater';
+
+SELECT Customers.FirstName, Customers.LastName, Dishes.Name AS FavoriteDish 
+FROM Customers 
+JOIN Dishes ON Customers.FavoriteDish = Dishes.DishID
+WHERE Customers.FirstName = 'Cleo' 
+  AND Customers.LastName = 'Goldwater';
